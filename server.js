ee782eec41dbd6f19910522d13e34be9284bb685
justkashish <meghanikashish1@gmail.com>
2025-04-27 17:22:35 +0530

    frontend url added

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://my-journal-plum-ten.vercel.app", // your frontend URL
+    credentials: true
+}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
